479d66aa0a13443454853f416cad386334e74f74
hamedbanaei <91188284+hamedbanaei@users.noreply.github.com>
2021-09-25 18:53:35 +0330

    Some other implementation has been added.

---
+++ Singleton/Singleton/01_SimpleSingleton.cs
@@ -0,0 +1,25 @@
+﻿namespace Implementation01_SimpleSingleton
+{
+	// You may use this implementation of Singleton for single thread programs.
+	// This implementation is the most simple one.
+	// It's useable when you want to use it in classes that you can reach their sources.
+	public sealed class Singleton
+	{
+		private static Singleton _instance = null;
+		public static Singleton Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new Singleton();
+				}
+				return (_instance);
+			}
+		}
+
+		private Singleton()
+		{
+		}
+	}
+}
---
+++ Singleton/Singleton/02_MultithreadSingletonWithLockObject.cs
@@ -0,0 +1,33 @@
+﻿namespace Implementation02_MultithreadSingletonWithLockObject
+{
+	public sealed class Singleton
+	{
+		private static readonly object _lockObject = new object();
+
+		private static Singleton _instance = null;
+		public static Singleton Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (_lockObject)
+					{
+						// It's more safe than using one if statement before lock.
+						// But we benefit static constructor and nested class ...
+						// for the best result (as shown in following implementations).
+						if (_instance == null)
+						{
+							_instance = new Singleton();
+						}
+					}
+				}
+				return (_instance);
+			}
+		}
+
+		private Singleton()
+		{
+		}
+	}
+}
