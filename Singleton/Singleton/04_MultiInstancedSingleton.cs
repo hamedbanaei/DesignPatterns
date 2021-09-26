508e5be9fc3d15005b72bfd92e9cf3ac0d190cce
hamedbanaei <91188284+hamedbanaei@users.noreply.github.com>
2021-09-26 13:24:40 +0330

    Multi Instance Singleton 04 and 05 has been added.

---
+++ Singleton/Singleton/04_MultiInstancedSingleton.cs
@@ -0,0 +1,30 @@
+﻿namespace Implementation04_MultiInstancedSingleton
+{
+	// The Multi-Instanced Singleton
+	// We have some printers (or any other concept) for managing...
+	public sealed class PrintManager
+	{
+		private static readonly System.Collections.Generic.Dictionary<string, PrintManager>
+			_instances = new System.Collections.Generic.Dictionary<string, PrintManager>();
+
+		public static PrintManager GetInstance(string printerName)
+		{
+			lock (_instances)
+			{
+				if (_instances.ContainsKey(printerName) == false)
+				{
+					_instances.Add(printerName, new PrintManager());
+				}
+			}
+			return (_instances[printerName]);
+		}
+
+		static PrintManager()
+		{
+		}
+
+		private PrintManager()
+		{
+		}
+	}
+}
