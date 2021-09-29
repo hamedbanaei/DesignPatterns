caf5f7abc752dbfe0d844f2c72f07b4c54d5b024
hamedbanaei <91188284+hamedbanaei@users.noreply.github.com>
2021-09-29 11:25:29 +0330

    StrategyPattern sample has been added

---
+++ StrategyPattern/StrategyPattern/FirstNameSorter.cs
@@ -0,0 +1,36 @@
+﻿namespace StrategyPattern
+{
+	public class FirstNameSorter : System.Collections.Generic.IComparer<Person>
+	{
+		public FirstNameSorter()
+		{
+			SortOrder = SortOrder.Ascending;
+		}
+
+		public FirstNameSorter(SortOrder sortOrder)
+		{
+			SortOrder = sortOrder;
+		}
+
+		public SortOrder SortOrder { get; set; }
+
+		public int Compare(Person x, Person y)
+		{
+			if (x == null || y == null)
+			{
+				return (0);
+			}
+			else
+			{
+				if (SortOrder == SortOrder.Ascending)
+				{
+					return (string.Compare(x.FirstName.ToLower(), y.FirstName.ToLower()));
+				}
+				else
+				{
+					return (string.Compare(y.FirstName.ToLower(), x.FirstName.ToLower()));
+				}
+			}
+		}
+	}
+}
---
+++ StrategyPattern/StrategyPattern/LastNameSorter.cs
@@ -0,0 +1,36 @@
+﻿namespace StrategyPattern
+{
+	public class LastNameSorter : System.Collections.Generic.IComparer<Person>
+	{
+		public LastNameSorter()
+		{
+			SortOrder = SortOrder.Ascending;
+		}
+
+		public LastNameSorter(SortOrder sortOrder)
+		{
+			SortOrder = sortOrder;
+		}
+
+		public SortOrder SortOrder { get; set; }
+
+		public int Compare(Person x, Person y)
+		{
+			if (x == null || y == null)
+			{
+				return (0);
+			}
+			else
+			{
+				if (SortOrder == SortOrder.Ascending)
+				{
+					return (string.Compare(x.LastName.ToLower(), y.LastName.ToLower()));
+				}
+				else
+				{
+					return (string.Compare(y.LastName.ToLower(), x.LastName.ToLower()));
+				}
+			}
+		}
+	}
+}
