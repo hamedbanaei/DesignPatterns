6325ae9fca422a2af419c15903c77e951f4365c3
Hamed Banaei <hamedbanaei1982@gmail.com>
2025-11-20 14:57:45 +0330

    Simple Builder Added.

---
+++ BuilderPattern/1_SimpleBuilder/HtmlElement.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace SimpleBuilder;
+
+public class HtmlElement
+{
+    public string Name { get; set; }
+    
+    public string Text { get; set; }
+    
+    public List<HtmlElement> Elements = new List<HtmlElement>();
+    private const int IndentSize = 4;
+
+    public HtmlElement()
+    {
+
+    }
+
+    public HtmlElement(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+
+    private string ToStringImpl(int indent)
+    {
+        var sb = new StringBuilder();
+        var i = new string(' ', IndentSize * indent);
+        sb.Append($"{i}<{Name}>\n");
+        if (!string.IsNullOrWhiteSpace(Text))
+        {
+            sb.Append(new string(' ', IndentSize * (indent + 1)));
+            sb.Append(Text);
+            sb.Append("\n");
+        }
+
+        foreach (var e in Elements)
+            sb.Append(e.ToStringImpl(indent + 1));
+
+        sb.Append($"{i}</{Name}>\n");
+        return sb.ToString();
+    }
+
+    public override string ToString()
+    {
+        return ToStringImpl(0);
+    }
+}
---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,51 @@
+﻿// ********** ********** ********** ********** **********
+// Life Without Builder
+// ********** ********** ********** ********** **********
+
+System.Console.WriteLine("Life Without Builder is like:");
+
+// if you want to build a simple HTML paragraph using StringBuilder
+string hello = "Hello";
+var sb = new System.Text.StringBuilder();
+sb.Append("<p>");
+sb.Append(hello);
+sb.Append("</p>");
+System.Console.WriteLine(sb);
+System.Console.WriteLine(System.Environment.NewLine);
+
+
+// now I want an HTML list with some words in it
+var words = new[] { "Hello", "Wise", "Students" };
+sb.Clear();
+sb.Append("<ul>");
+foreach (var item in words)
+{
+    sb.Append($"<li>{item}</li>");
+
+}
+sb.Append("</ul>");
+System.Console.WriteLine(sb);
+System.Console.WriteLine(System.Environment.NewLine);
+
+// ********** ********** ********** ********** **********
+// / Life Without Builder
+// ********** ********** ********** ********** **********
+
+
+
+// ********** ********** ********** ********** **********
+// Simple Builder
+// ********** ********** ********** ********** **********
+
+System.Console.WriteLine("Ordinary non-fluent Builder is like:");
+// Ordinary non-fluent Builder
+var builder = new SimpleBuilder.HtmlBuilder("ul");
+builder.AddChild("li", "hello");
+builder.AddChild("li", "world");
+System.Console.WriteLine(builder.ToString());
+System.Console.WriteLine(System.Environment.NewLine);
+
+// ********** ********** ********** ********** **********
+// / Simple Builder
+// ********** ********** ********** ********** **********
+
